25aa3e57c4a5f0037ce73d5c99aa7ea3503611d0
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2025-11-04 17:06:03 +0100

    push final

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,15 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+aMettreEnRouge.style.color="red";
 
+const colH2 = document.querySelectorAll("h2");
+colH2.forEach((h2)=>{
+    h2.addEventListener("click",()=>{;
+    h2.style.color="red";
+    });
+});
 
+const enRougeSuiteAUnClic = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAUnClic.addEventListener("click",(element)=>{
+        enRougeSuiteAUnClic.style.color="red";
+    });
